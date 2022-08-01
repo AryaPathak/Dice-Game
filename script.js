2e99851552a810e5a1b3162bfd3fc24c8f06cd4a
arya <a@gmail.com>
2022-08-01 12:38:24 +0530

    Added SourceCode

---
+++ script.js
@@ -0,0 +1,92 @@
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+
+
+let scores, currentScore, activePlayer, playing;
+
+const init = function(){
+   scores = [0,0];
+   currentScore=0;
+   activePlayer = 0;
+   playing = true;
+
+  score0El.textContent=0;
+  score1El.textContent=0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+}
+
+init();
+
+btnRoll.addEventListener('click', function(){
+  if(playing){
+    //rollig dice
+    const dice = Math.trunc(Math.random()*6)+1;
+
+    //Display Dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    //Check for 1: if true
+    if(dice != 1){
+      //Add to current score
+      currentScore+=dice;
+      document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    }else{
+      //switch the Player
+      document.getElementById(`current--${activePlayer}`).textContent = 0;
+      activePlayer = activePlayer === 0 ? 1 : 0;
+      currentScore=0;
+      player0El.classList.toggle('player--active');
+      player1El.classList.toggle('player--active');
+    }
+  }
+
+})
+
+btnHold.addEventListener('click', function(){
+
+  if(playing){
+    //Add current score to active users global scores
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+    //check if player's score >= 30
+    if(scores[activePlayer] >= 30){
+      playing = false;
+      document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+      document.querySelector(`player--${activePlayer}`).classList.remove('player--active');
+    }else{
+      //Switch to next player
+      document.getElementById(`current--${activePlayer}`).textContent = 0;
+
+      activePlayer = activePlayer === 0 ? 1 : 0;
+      currentScore=0;
+      player0El.classList.toggle('player--active');
+      player1El.classList.toggle('player--active');
+    }
+
+  }
+
+
+
+
+})
+
+btnNew.addEventListener('click', init);
